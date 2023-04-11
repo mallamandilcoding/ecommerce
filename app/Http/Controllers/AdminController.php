57d610db7207a57c85ebd97f1bd2b94ff066768c
Mandil Malla <mallamandilcoding@gmail.com>
2023-04-12 00:14:37 +0900

    logout of admin added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
+    }
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function AdminLogin(){
+        return view('admin.login');
     }
 }

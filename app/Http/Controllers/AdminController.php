f5ce8bf6f43c44e409f8fd00044e24dc5f5c9d85
Mandil Malla <mallamandilcoding@gmail.com>
2023-04-12 19:39:42 +0900

    edit of admin done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AdminController extends Controller
@@ -26,5 +27,30 @@ class AdminController extends Controller
 
     public function AdminLogin(){
         return view('admin.login');
+    }
+
+    public function AdminProfile(){
+        $id= Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+    }
+
+    public function AdminProfileStore(Request $req){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $req->name;
+        $data->email = $req->email;
+        $data->phone = $req->phone;
+        $data->address = $req->address;
+        if ($req->file('photo')){
+            $file = $req->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            //upload image to public folder
+            $file->move(public_path('upload/admin_images'),$filename);
+            //save to db
+            $data['photo'] = $filename;
+        }
+        $data->save();
+        return redirect()->back();
     }
 }

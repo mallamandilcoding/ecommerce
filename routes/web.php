49c11bd583ec1a93d3fa743f14ff4d16f4dc516b
Mandil Malla <mallamandilcoding@gmail.com>
2023-04-13 18:23:10 +0900

    frontend template added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\VendorController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.master_dashboard');
 });
 
 Route::get('/dashboard', function () {

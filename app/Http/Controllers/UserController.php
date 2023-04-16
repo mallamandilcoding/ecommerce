c39d5e10f663efa594112a92656b3c107f8e783b
Mandil Malla <mallamandilcoding@gmail.com>
2023-04-16 13:26:41 +0900

    user dashboard updated

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+
+class UserController extends Controller
+{
+    public function UserDashboard(){
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+        return view('user_dashboard',compact('userData'));
+    }
+
+    public function UserDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
